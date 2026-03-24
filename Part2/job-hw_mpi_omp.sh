5a5f3fc9f5bee010a86f26f34c046df7b1e43006
Adam Jacobs <adam.jacobs@login7.ufhpc>
2026-03-24 14:47:32 -0400

    added output

--- Part2/job-hw_mpi_omp.sh
+++ Part2/job-hw_mpi_omp.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --account=eel6763
 #SBATCH --qos=eel6763
-#SBATCH --nodes=4
+#SBATCH --nodes=1
 #SBATCH --ntasks=8
-#SBATCH --ntasks-per-node=2
+#SBATCH --ntasks-per-node=8
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=1000mb
 #SBATCH -t 00:05:00
-#SBATCH -o Part2_Table2_Row3
+#SBATCH -o test
 #SBATCH -e errfile
 export OMP_NUM_THREADS=8
 srun --mpi=$HPC_PMIX ./a.out
